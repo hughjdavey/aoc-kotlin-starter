13eb3aafeb8c8e629a385124c0c9f0f9b4141c63
James Baker <j_d_baker@hotmail.com>
2021-08-08 15:16:17 +0100

    Upgrade Kotlin to 1.5.21 (#2)
    Replace deprecated String#toUpperCase calls with String#uppercase
    Thanks to @RusticFlare

--- src/main/kotlin/days/Day1.kt
+++ src/main/kotlin/days/Day1.kt
@@ -4,14 +4,14 @@ class Day1 : Day(1) {
 
     override fun partOne(): Any {
         return inputList.take(2)
-            .map { it.toUpperCase() }
+            .map { it.uppercase() }
             .joinToString(" ")
     }
 
     override fun partTwo(): Any {
         return inputString.split("\n")
             .filterNot { it.isEmpty() }
-            .map { it.toUpperCase() }
+            .map { it.uppercase() }
             .last()
     }
 }
