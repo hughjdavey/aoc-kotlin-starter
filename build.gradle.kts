1c9deb0f98eac9dd31b961fd9fd6df5a8b3f346b
hughjdavey <9026053+hughjdavey@users.noreply.github.com>
2023-11-23 08:39:22 +0000

    2023 update
    Update kotlin to 1.9.20 and gradle to 8.4
    Also update slf4j and junit libraries to latest versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,6 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
     application
-    kotlin("jvm") version "1.7.20"
+    kotlin("jvm") version "1.9.20"
 }
 
 application {
@@ -18,18 +16,18 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation("org.reflections", "reflections", "0.10.2")
-    implementation("org.slf4j:slf4j-nop:2.0.4")
+    implementation("org.slf4j:slf4j-nop:2.0.9")
 
-    testImplementation("org.junit.jupiter", "junit-jupiter-api", "5.9.1")
-    testImplementation("org.junit.jupiter", "junit-jupiter-engine", "5.9.1")
+    testImplementation("org.junit.jupiter", "junit-jupiter-api", "5.10.1")
+    testImplementation("org.junit.jupiter", "junit-jupiter-engine", "5.10.1")
     testImplementation("org.hamcrest", "hamcrest", "2.2")
     testImplementation ("com.github.stefanbirkner:system-lambda:1.2.1")
 }
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+tasks.test {
+    useJUnitPlatform()
 }
 
-tasks.withType<Test> {
-    useJUnitPlatform()
+kotlin {
+    jvmToolchain(20)
 }
