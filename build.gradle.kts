e7e0e583330b3a7414f736b601b3be36f896ac1c
hughjdavey <9026053+hughjdavey@users.noreply.github.com>
2024-11-29 11:33:28 +0000

    2024 update
    Update kotlin to 2.1.0 and gradle to 8.11.1
    Update jvmToolchain version to 21
    Also update slf4, junit and hamcrest libraries to latest versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     application
-    kotlin("jvm") version "1.9.20"
+    kotlin("jvm") version "2.1.0"
 }
 
 application {
@@ -16,11 +16,11 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation("org.reflections", "reflections", "0.10.2")
-    implementation("org.slf4j:slf4j-nop:2.0.9")
+    implementation("org.slf4j:slf4j-nop:2.0.16")
 
-    testImplementation("org.junit.jupiter", "junit-jupiter-api", "5.10.1")
-    testImplementation("org.junit.jupiter", "junit-jupiter-engine", "5.10.1")
-    testImplementation("org.hamcrest", "hamcrest", "2.2")
+    testImplementation("org.junit.jupiter", "junit-jupiter-api", "5.11.3")
+    testImplementation("org.junit.jupiter", "junit-jupiter-engine", "5.11.3")
+    testImplementation("org.hamcrest", "hamcrest", "3.0")
     testImplementation ("com.github.stefanbirkner:system-lambda:1.2.1")
 }
 
@@ -29,5 +29,5 @@ tasks.test {
 }
 
 kotlin {
-    jvmToolchain(20)
+    jvmToolchain(21)
 }
